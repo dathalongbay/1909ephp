e71649ba16ea882f1755bcc254b091ef8017e5e5
dat <dathalongbay@gmail.com>
2019-11-21 19:17:29 +0700

    111

---
+++ day15/001/index6.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+    <pre>
+        Các kiểu dữ liệu trong PHP
+        Học hàm debug trong PHP
+        var_dump() in ra giá trị của biến và kiểu dữ liệu của biến đó
+    </pre>
+
+    <?php
+    // kiểu số
+    $a = 5;
+    echo "<br>";
+    var_dump($a);
+    // kiểu chuỗi
+    $b = "abc";
+    echo "<br>";
+    var_dump($b);
+    // kiểu số thực
+    $c = 3.5;
+    echo "<br>";
+    var_dump($c);
+    // Boolean
+    $d = true;
+    $e = false;
+    echo "<br>";
+    var_dump($d);
+
+    echo "<br>";
+    var_dump($e);
+
+    // Mảng trong PHP
+    // Object đối tượng trong php
+
+    // kiểu null
+    $f = null;
+
+    echo "<br>";
+    var_dump($f);
+
+    ?>
+
+
+</body>
+</html>
