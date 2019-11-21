9cf0f6a310c89c578a59c878dcb74b5dff4eebe1
dat <dathalongbay@gmail.com>
2019-11-21 18:52:24 +0700

    111

---
+++ day15/001/index3.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+    <pre>
+        Chú ý về quy tắc nháy kép chứa nháy đơn
+        và nháy đơn chứa nháy kép
+        cũng như ký tự escape khi xuất chuỗi
+
+        Yêu cầu :
+        In ra câu nói của bác hồ
+        "Không có gì quý hơn độc lập tự do"
+    </pre>
+
+    <?php
+    // lệnh php
+    // cách 1 sử dụng nháy đơn chứa nháy kép
+    echo '"Không có gì quý hơn độc lập tự do"';
+    echo "<br>";
+    // cách 2 sử dụng ký tự escape \ để php hiểu đâu là nháy kép thuộc chuỗi
+    echo "\"Không có gì quý hơn độc lập tự do\"";
+    ?>
+
+</body>
+</html>
