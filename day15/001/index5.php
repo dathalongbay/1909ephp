6a708b7d61986e4e4047a6f480b4877794b4c090
dat <dathalongbay@gmail.com>
2019-11-21 19:06:23 +0700

    1

---
+++ day15/001/index5.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+    <pre>
+        Biến trong php
+        Biến php bắt $
+        trong php nối chuỗi thì dùng dấu .
+    </pre>
+
+    <?php
+    $a = 5;
+    $b = 2;
+    echo "<br> tổng " . ($a + $b);
+    echo "<br> hiệu " . ($a - $b);
+    echo "<br> tích " . ($a * $b);
+    echo "<br> thương " . ($a / $b);
+    echo "<br> phép chia lấy dư " . ($a % $b);
+    ?>
+</body>
+</html>
