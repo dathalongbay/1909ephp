64a0fae7fa427f7d6698f3bf54b6920f9b7e22ee
dat <dathalongbay@gmail.com>
2019-11-21 18:36:55 +0700

    111

---
+++ day15/001/index2.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+    <h1>Nhúng mã PHP và trong mã html</h1>
+    <pre>
+        Tất cả các lệnh php phải được đặt trong &lt;?php ?&gt;
+        Mã mở của php &lt;?php
+        Mã đóng ?&gt;
+    </pre>
+
+    <?php
+    echo "Tất cả các lệnh php phải được đặt trong cú pháp &lt;?php ?&gt;";
+    echo "<br>";
+    echo "Các lệnh php đều kết thúc bằng dấu ;";
+    ?>
+
+
+</body>
+</html>
