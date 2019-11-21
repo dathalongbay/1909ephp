c1b11fd4474d89122bef1ec3ca83fb4725bcba86
dat <dathalongbay@gmail.com>
2019-11-21 20:56:02 +0700

    111

---
+++ day15/004/index1.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: admin
+ * Date: 11/21/2019
+ * Time: 8:48 PM
+ */
+
+// cú pháp thu gọn của if else
+$tuoi = 21;
+if ($tuoi > 18) {
+    echo "nguoi lon";
+} else {
+    echo "tre em";
+}
+
+echo "<br>";
+echo ($tuoi > 18) ? "nguoi lon" : "tre em";
