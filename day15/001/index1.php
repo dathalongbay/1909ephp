57e3bbdabebeb5ff907e2d5e4ad325fcecda3281
dat <dathalongbay@gmail.com>
2019-11-21 18:22:05 +0700

    111

---
+++ day15/001/index1.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: admin
+ * Date: 11/21/2019
+ * Time: 6:16 PM
+ */
+
+// Chú thích 1 dòng
+
+/**
+ * Chú thích nhiều dòng
+ * Comment giải thích code
+ * Câu lệnh xuất ra màn hình
+ *
+ * Ghi nhớ lệnh echo xuất dữ liệu ra màn hình
+ * và nếu mà xuất chuỗi thì phải đặt "" hay ''
+ */
+echo "Hello PHP trong nháy kép";
+echo "<br>";
+echo 'Hello PHP trong nháy đơn';
+echo "<br>";
+echo "<h1>In ra thẻ h1 bằng lệnh PHP</h1>";
+echo 2000;
