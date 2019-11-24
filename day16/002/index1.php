925ec87af67ee52158451eea9ac32c2736d18eb6
dat <dathalongbay@gmail.com>
2019-11-24 19:47:29 +0700

    111

---
+++ day16/002/index1.php
@@ -0,0 +1,12 @@
+<?php
+$cars = array("Volvo", "BMW", "Toyota");
+
+echo "<pre>";
+print_r($cars);
+echo "</pre>";
+
+sort($cars);
+
+echo "<pre>";
+print_r($cars);
+echo "</pre>";
---
+++ day16/002/index3.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: admin
+ * Date: 11/24/2019
+ * Time: 7:44 PM
+ */
+
+$age = array("Peter"=>"35", "Ben"=>"37", "Joe"=>"43");
+
+echo "<pre>";
+print_r($age);
+echo "</pre>";
+asort($age);
+
+echo "<pre>";
+print_r($age);
+echo "</pre>";
