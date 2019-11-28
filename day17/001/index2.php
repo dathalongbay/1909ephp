6110850c207c2aac7bb28e3d9179cab67456a576
dat <dathalongbay@gmail.com>
2019-11-28 19:18:22 +0700

    111

---
+++ day17/001/index2.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+</head>
+<body>
+
+<pre>
+    Tạo 1 form đăng nhập trong chính file index2.php
+    có username và mật khẩu
+    nếu username = admin và mật khẩu == 12345
+    thì hiển thị ra màn hình 3 dòng thông báo thành công bên dưới
+    chú ý không được echo ra thẻ html để hiểu được cách nhúng thẻ html
+    trong các cú pháp if else hay for loop trong PHP
+    ngược lại hiển thị 3 dòng thông báo thất bại trong chính file này
+
+
+</pre>
+
+<div class="alert alert-success">
+    <strong>Success! Bạn đã đăng nhập thành công</strong>
+</div>
+
+<div class="alert alert-danger">
+    <strong>Fail!</strong> Sai username và mật khẩu
+</div>
+
+</body>
+</html>
