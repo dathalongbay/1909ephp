64438041ddf64cb9622b2305db0e9b8f213e5ae0
dat <dathalongbay@gmail.com>
2019-12-01 20:57:39 +0700

    111

---
+++ day18/008/index1.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+    <pre>
+        $_SERVER is a PHP super global variable which holds information about headers, paths, and script locations.
+        chứa thông tin về headers, đường dẫn và vị trí script
+    </pre>
+
+    <?php 
+    echo "<pre>";
+    print_r($_SERVER);
+    echo "</pre>";
+    ?>
+</body>
+</html>
