13dbbec5871ec7ef298d1963727e487bd0922627
dat <dathalongbay@gmail.com>
2019-12-12 20:10:05 +0700

    111

--- day21/001/Utilities/Payment/payment.php
+++ day21/001/Utilities/Payment/payment.php
@@ -1,7 +1,14 @@
 <?php
 namespace Utilities\Payment;
 
+// nạp namespace cho trait
+use Traits\Momo;
+use Traits\Napas;
+
 class Payment {
+
+    // nạp trait đa kế thừa
+    use Momo, Napas;
 
     public function demo() {
         echo "<br>" . __METHOD__;
--- day21/001/index.php
+++ day21/001/index.php
@@ -1,4 +1,17 @@
 <?php
+/**
+ * Bước 1 nạp tất cả các file chứa các class
+ * include_once , include , require , require_once
+ * include_once "đường dẫn file";
+ */
+include_once "Excel/import.php";
+include_once "Excel/export.php";
+include_once "Traits/momo.php";
+include_once "Traits/napas.php";
+include_once "Utilities/Payment/payment.php";
+include_once "Utilities/Paypal/paypal.php";
+include_once "Utilities/Pdf/pdf.php";
+
 
 
 /**
@@ -11,3 +24,37 @@
  * cuối cùng hãy dùng chính đối tượng payment khởi tạo từ class Payment
  * và gọi đến 2 method demoMomo và demoNapas từ đối tượng payment
  */
+
+/**
+ * Bước 2 import 5 namespace
+ */
+use \Excel\ExportExcel;
+use \Excel\ImportExcel;
+use \Utilities\Payment\Payment;
+use \Utilities\Paypal\Paypal;
+use \Utilities\Pdf\Pdf;
+
+/**
+ * Bước 3 khởi tạo đối tượng
+ */
+$exportExcel = new ExportExcel();
+$importExcel = new ImportExcel();
+$payment = new Payment();
+$paypal = new Paypal();
+$pdf = new Pdf();
+
+/**
+ * Bước gọi method demo()
+ */
+$exportExcel->demo();
+$importExcel->demo();
+$payment->demo();
+$paypal->demo();
+$pdf->demo();
+
+$payment->demoMomo();
+$payment->demoNapas();
+
+
+
+
