0bb3ed1da26f8991d3f2c2e336232fc202011782
dat <dathalongbay@gmail.com>
2019-12-19 19:26:27 +0700

    dsfdsf

--- day23/002/index3.php
+++ day23/002/index3.php
@@ -13,9 +13,10 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
+$dbname = "ngay19thang12";
 
 try {
-    $conn = new PDO("mysql:host=$servername;dbname=ngay19thang12", $username, $password);
+    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
     // set the PDO error mode to exception
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     echo "Connected thành công";
