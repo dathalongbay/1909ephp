869d7190d9381d5bb42eff3a3053bedea57caaed
dat <dathalongbay@gmail.com>
2019-12-19 19:16:52 +0700

    dfdsfsdfdsff

---
+++ day23/002/index3.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: admin
+ * Date: 12/19/2019
+ * Time: 7:13 PM
+ */
+
+/**
+ * Cách số 3 sử dụng PDO để kết nối đến mysql
+ */
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+
+try {
+    $conn = new PDO("mysql:host=$servername;dbname=ngay19thang12", $username, $password);
+    // set the PDO error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connected thành công";
+}
+catch(PDOException $e)
+{
+    echo "Connection thất bại: " . $e->getMessage();
+}
+
+echo "DEMO TEST";
