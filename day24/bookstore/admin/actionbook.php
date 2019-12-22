ccf24488fcb3e71078980424894e9f87bb2b03c9
dat <dathalongbay@gmail.com>
2019-12-22 20:36:40 +0700

    sadsad

--- day24/bookstore/admin/actionbook.php
+++ day24/bookstore/admin/actionbook.php
@@ -1,5 +1,5 @@
 <?php
-
+include_once "../config/connectdb.php";
 if (isset($_POST["action"])) {
     if ($_POST["action"] == "edit") {
         // code edit
@@ -15,16 +15,47 @@ if (isset($_POST["action"])) {
             "book_images", "book_price", "book_sell", "book_author", "book_publisher",
             "created" , "updated", "book_hit", "book_buy", "book_status", "book_desc"];
 
+        $bookFieldsNotQuote = ["book_hit", "book_buy", "book_status", "book_price", "book_sell"];
+
         $data = [];
         foreach($bookFields as $fieldName) {
             if (isset($_POST[$fieldName])) {
-                $data[$fieldName] = $_POST[$fieldName];
+
+                if (!in_array($fieldName, $bookFieldsNotQuote)) {
+                    $data[$fieldName] = "'".$_POST[$fieldName]."'";
+                } else {
+                    $data[$fieldName] = $_POST[$fieldName];
+                }
+            } else {
+                if ($fieldName == "created" || $fieldName == "updated") {
+                    if (!in_array($fieldName, $bookFieldsNotQuote)) {
+                        $data[$fieldName] = "'".date("Y-m-d H:i:s")."'";
+                    } else {
+                        $data[$fieldName] = date("Y-m-d H:i:s");
+                    }
+
+                }
             }
+
         }
 
         echo "<pre>";
         print_r($data);
-        echo "</pre>"; die;
+        echo "</pre>";
+
+        // convert mảng thành 1 chuỗi
+        $fieldsSQL = implode(',', $bookFields);
+        $fieldsValue = implode(",", $data);
+
+        echo "<br>" . $fieldsSQL;
+        echo "<br>" . $fieldsValue;
+
+        $sqlInsert = "INSERT INTO books($fieldsSQL) VALUES ($fieldsValue)";
+        echo "<br>" . $sqlInsert;
+
+        $insertFlag = $conn->query($sqlInsert);
+        var_dump($insertFlag);
+        die;
     }
 }
 
