9d403f8a696207c05272e5760f96d5495c78deaf
dat <dathalongbay@gmail.com>
2020-01-05 19:49:43 +0700

    dsfdsf

--- day27mvc/demo/index.php
+++ day27mvc/demo/index.php
@@ -1,7 +1,9 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: admin
- * Date: 1/5/2020
- * Time: 6:34 PM
- */
+
+// Nạp tất cả các file khác vào trong luồng chạy index.php
+include_once "mvc/route.php";
+include_once "mvc/models/Database.php";
+include_once "mvc/controllers/PostController.php";
+include_once "mvc/controllers/ProductController.php";
+include "mvc/models/PostModel.php";
+include "mvc/models/ProductModel.php";
