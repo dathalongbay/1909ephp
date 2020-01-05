472a01820a36a1607aa32706efefd15fe300dc51
dat <dathalongbay@gmail.com>
2020-01-05 20:44:31 +0700

    dsfsdf

--- day27mvc/demo/mvc/controllers/PostController.php
+++ day27mvc/demo/mvc/controllers/PostController.php
@@ -6,21 +6,29 @@ class PostController {
     public function indexAction() {
         // __METHOD__ in ra tên class và tên method đang được gọi đến
         echo "<br>" . __METHOD__;
+
+        include_once "mvc/views/post/index.php";
     }
 
     public function createAction() {
 // __METHOD__ in ra tên class và tên method đang được gọi đến
         echo "<br>" . __METHOD__;
+
+        include_once "mvc/views/post/create.php";
     }
 
     public function editAction() {
 // __METHOD__ in ra tên class và tên method đang được gọi đến
         echo "<br>" . __METHOD__;
+
+        include_once "mvc/views/post/edit.php";
     }
 
     public function deleteAction() {
 // __METHOD__ in ra tên class và tên method đang được gọi đến
         echo "<br>" . __METHOD__;
+
+        include_once "mvc/views/post/delete.php";
     }
 
 }
--- day27mvc/demo/mvc/controllers/ProductController.php
+++ day27mvc/demo/mvc/controllers/ProductController.php
@@ -7,21 +7,29 @@ class ProductController {
     public function indexAction() {
 // __METHOD__ in ra tên class và tên method đang được gọi đến
         echo "<br>" . __METHOD__;
+
+        include_once "mvc/views/product/index.php";
     }
 
     public function createAction() {
 // __METHOD__ in ra tên class và tên method đang được gọi đến
         echo "<br>" . __METHOD__;
+
+        include_once "mvc/views/product/create.php";
     }
 
     public function editAction() {
 // __METHOD__ in ra tên class và tên method đang được gọi đến
         echo "<br>" . __METHOD__;
+
+        include_once "mvc/views/product/edit.php";
     }
 
     public function deleteAction() {
 // __METHOD__ in ra tên class và tên method đang được gọi đến
         echo "<br>" . __METHOD__;
+
+        include_once "mvc/views/product/delete.php";
     }
 
 }
