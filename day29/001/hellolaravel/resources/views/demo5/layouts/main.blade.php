75704a85dba48d83aad733cbded277bc271a931c
dat <dathalongbay@gmail.com>
2020-02-13 20:19:30 +0700

    111

---
+++ day29/001/hellolaravel/resources/views/demo5/layouts/main.blade.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+
+    <style type="text/css">
+        * {
+            padding: 0;
+            margin: 0;
+        }
+
+        header {
+            height: 150px;
+            background-color: green;
+        }
+
+        footer {
+            height: 200px;
+            background-color: olive;
+            clear: both;
+        }
+
+        nav {
+            background-color: blue;
+        }
+
+        nav ul li {
+            display: inline-block;
+            background-color: purple;
+        }
+
+
+
+        main #mainpage {
+            float: left;
+            width: 80%;
+            background-color: #0C9A9A;
+            height: 500px;
+        }
+
+        main aside {
+            float: left;
+            width: 18%;
+            background-color: pink;
+            height: 500px;
+        }
+    </style>
+</head>
+<body>
+
+@include("partials.header");
+
+
+@include("partials.navigation");
+
+
+<main>
+    <section id="mainpage">
+        <h1>Khai báo vị trí để load sub view</h1>
+        @yield("content")
+    </section>
+    @include("partials.sidebar");
+</main>
+
+@include("partials.footer");
+
+</body>
+</html>
---
+++ day29/001/hellolaravel/resources/views/demo5/partials/navigation.blade.php
@@ -0,0 +1,7 @@
+<nav>
+    <ul>
+        <li><a href="">Trang chủ</a> </li>
+        <li><a href="">Giới thiệu</a> </li>
+        <li><a href="">Liên hệ</a> </li>
+    </ul>
+</nav>
