4458cb47262cbfae97c35a58b04156b1e3b5f3ba
dat <dathalongbay@gmail.com>
2020-02-16 18:34:33 +0700

    dsfdsfdsfdsf

---
+++ day31/bookstore/app/Models/Backend/BooksModel.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Models\Backend;
+
+use Illuminate\Database\Eloquent\Model;
+
+class BooksModel extends Model
+{
+    //
+}
---
+++ day31/bookstore/app/Models/Frontend/BooksModel.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Models\Frontend;
+
+use Illuminate\Database\Eloquent\Model;
+
+class BooksModel extends Model
+{
+    //
+}
