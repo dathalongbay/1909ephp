854201daed739db6026b28a439518ed524f156e5
dat <dathalongbay@gmail.com>
2020-05-10 18:29:00 +0700

    dfdsfdsf

---
+++ day31/bookstore/app/Http/Controllers/Backend/LearndbController.php
@@ -0,0 +1,152 @@
+<?php
+
+namespace App\Http\Controllers\Backend;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class LearndbController extends Controller
+{
+    //
+
+    public function index($action) {
+
+        $this->$action();
+    }
+
+
+    /*
+     * Lấy ra tất cả các bản ghi trong 1 bảng
+     * */
+    public function demo1() {
+
+    }
+
+
+    /*
+     * Lấy ra 1 bản ghi duy nhất trong 1 bảng cách 1
+     * */
+    public function demo2() {
+
+    }
+
+
+    /*
+     * Lấy ra 1 bản ghi duy nhất trong 1 bảng cách 2
+     * */
+    public function demo3() {
+
+    }
+
+    /*
+     * Lấy ra 1 danh sách các cột trong bảng
+     * */
+    public function demo4() {
+
+    }
+
+
+    /*
+     * count() max() avg()
+     * */
+    public function demo5() {
+
+    }
+
+    /*
+    * select db
+    * */
+    public function demo6() {
+
+    }
+
+
+    /*
+    * Raw query
+    * */
+    public function demo7() {
+
+    }
+
+
+    /*
+    * Join laravel
+    * */
+    public function demo8() {
+
+    }
+
+
+    /*
+    * Join laravel
+    * */
+    public function demo9() {
+
+    }
+
+
+    /*
+    * Where 1
+    * */
+    public function demo10() {
+
+    }
+
+
+    /*
+   * Where 2
+   * */
+    public function demo11() {
+
+    }
+
+
+    /*
+  * Order
+  * */
+    public function demo12() {
+
+    }
+
+
+    /*
+  * Group
+  * */
+    public function demo13() {
+
+    }
+
+
+    /*
+  * Inssert
+  * */
+    public function demo14() {
+
+    }
+
+
+
+    /*
+  * Update
+  * */
+    public function demo15() {
+
+    }
+
+
+    /*
+ * Delete
+ * */
+    public function demo16() {
+
+    }
+
+
+
+    /*
+ * Bebug
+ * */
+    public function demo17() {
+
+    }
+}
