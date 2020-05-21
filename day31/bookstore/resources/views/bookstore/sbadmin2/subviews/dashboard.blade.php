cf5d84146d020d4cb04f9bf63cd3cc0d3c0a35e6
dat <dathalongbay@gmail.com>
2020-05-21 20:16:29 +0700

    111

--- day31/bookstore/resources/views/bookstore/sbadmin2/subviews/dashboard.blade.php
+++ day31/bookstore/resources/views/bookstore/sbadmin2/subviews/dashboard.blade.php
@@ -1,3 +1,8 @@
+@extends("bookstore.sbadmin2.layouts.main")
+
+@section('title', 'Dashboard')
+
+@section('content')
 <!-- Page Heading -->
 <div class="d-sm-flex align-items-center justify-content-between mb-4">
     <h1 class="h3 mb-0 text-gray-800">Dashboard</h1>
@@ -275,3 +280,5 @@
 
     </div>
 </div>
+
+@endsection
